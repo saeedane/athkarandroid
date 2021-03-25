2504633e76c6bd3a74b6f64acdd34653e23a66ab
saeedane <dvdplay405@gmail.com>
2021-03-25 13:57:45 +0100

    upload code edit

--- app/src/main/java/com/example/adkar_moslim/DetailsActivity.java
+++ app/src/main/java/com/example/adkar_moslim/DetailsActivity.java
@@ -3,68 +3,56 @@ package com.example.adkar_moslim;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+import java.util.Random;
 
 public class DetailsActivity extends AppCompatActivity {
     int current_index = 0;
-    TextView tv_title;
+    TextView tv_title,tv_count;
     ImageView imageNext,imagePrev;
+    String[] athkar_sabah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         tv_title = findViewById(R.id.tv_title);
+        tv_count = findViewById(R.id.title_count);
         imageNext = findViewById(R.id.imageButton);
         imagePrev = findViewById(R.id.button_prev);
         showAthkarMoslim();
+
 
 
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     public void next_citation_morning(View view) {
-
-
-            if (current_index < 17) {
-
-                current_index++;
-                showAthkarMoslim();
-                Drawable image = getResources().getDrawable(R.drawable.rounded_rectangle_right);
-                imagePrev.setBackground(image);
-
-
-
-            }else{
-
-                Drawable image = getResources().getDrawable(R.drawable.rounded_rectangle_left_disabled);
-                imageNext.setBackground(image);
-                showAthkarMoslim();
-
-
-            }
-
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    public void prev_evening_prayers(View view) {
-
-
-        if(current_index > 0){
-
-            current_index--;
+        if(current_index < 17){
+            save_current_index();
+            current_index++;
             showAthkarMoslim();
+
+
             Drawable image = getResources().getDrawable(R.drawable.rounded_rectangle_left);
-            imageNext.setBackground(image);
-
-
-
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                imageNext.setBackground(image);
+            }
 
 
         }else{
@@ -72,22 +60,65 @@ public class DetailsActivity extends AppCompatActivity {
             Drawable image = getResources().getDrawable(R.drawable.rounded_rectangle_right_disabled);
             imagePrev.setBackground(image);
         }
+
+
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public void prev_evening_prayers(View view) {
+    try {
+        if(current_index > 0){
+            save_current_index();
+            current_index--;
+            showAthkarMoslim();
+
+
+            Drawable image = getResources().getDrawable(R.drawable.rounded_rectangle_left);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                imageNext.setBackground(image);
+            }
+
+
+        }else{
+
+            Drawable image = getResources().getDrawable(R.drawable.rounded_rectangle_right_disabled);
+            imagePrev.setBackground(image);
+        }
+
+    }catch (Exception e){e.getMessage();}
+
+
+
+    }
+    public void save_current_index(){
+
+        SharedPreferences preferences = getSharedPreferences("app_key", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("current_index_save",current_index);
+        editor.apply();
+
+
+
+    }
 
     private  void showAthkarMoslim(){
         int position = getIntent().getIntExtra("position",0);
+        SharedPreferences preferences = getSharedPreferences("app_key",Context.MODE_PRIVATE);
+        int get_current_index = preferences.getInt("current_index_save",0);
+
         if(position == 0){
 
             String[] athkar_sabah = getResources().getStringArray(R.array.athkar_sabah);
-            tv_title.setText(athkar_sabah[current_index]);
+            String[] athkar_count = getResources().getStringArray(R.array.athkar_count);
+            tv_title.setText(athkar_sabah[get_current_index]);
+            tv_count.setText(athkar_count[get_current_index]);
 
 
         }else if(position == 1){
 
             String[] athkar_masah = getResources().getStringArray(R.array.athkar_masah);
 
-            tv_title.setText(athkar_masah[current_index]);
+            tv_title.setText(athkar_masah[get_current_index]);
         }else{
             tv_title.setText(" ادعية اسلامية تقرب انسان من ربه ");
 
@@ -96,4 +127,31 @@ public class DetailsActivity extends AppCompatActivity {
 
 
     }
+
+
+    public void button_back_activity(View view) {
+
+
+        onBackPressed();
+
+
+
+
+    }
+
+
+
+    public void image_share_question(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,tv_title.getText().toString());
+        String message = getResources().getString(R.string.text_share_title);
+        startActivity(Intent.createChooser(intent,message));
+
+
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/adkar_moslim/MainActivity.java
+++ app/src/main/java/com/example/adkar_moslim/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
@@ -51,6 +54,60 @@ public class MainActivity extends AppCompatActivity  {
          title = findViewById(R.id.title);
         recyclerView = findViewById(R.id.recycler);
 
+
+    }
+
+    public void button_change_language(View view) {
+
+        showDialogLang();
+
+
+
+
+    }
+
+
+    private void showDialogLang() {
+        String[] charLang ={"ar", "en"};
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(R.string.changeLang);
+        dialog.setIcon(R.drawable.icon_1);
+        dialog.setItems(charLang, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0){
+                    LocaleHelper.setLocale(getApplicationContext(),"ar");
+                    refreshActivity();
+                    Toast.makeText(getApplicationContext(), "Locale in Arabic !", Toast.LENGTH_LONG).show();
+
+
+                }else{
+
+                    LocaleHelper.setLocale(getApplicationContext(),"en");
+                    refreshActivity();
+
+                    Toast.makeText(getApplicationContext(), "Locale in English !", Toast.LENGTH_LONG).show();
+
+
+                }
+
+
+            }
+        });
+
+        dialog.create();
+        dialog.show();
+
+
+    }
+
+
+    private void refreshActivity(){
+
+        Intent refresh = new Intent(getApplicationContext(), MainActivity.class);
+        refresh.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        refresh.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(refresh);
 
     }
 
